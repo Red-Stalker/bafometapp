1736120256350df35727abf60df2016b51060bc9
Danirill <dzykin3@gmail.com>
2020-10-24 18:31:55 +0500

    prime

---
+++ src/components/Sidebar/cardCompany/CardCompany.js
@@ -0,0 +1,16 @@
+import React, {useContext, useEffect, useState} from "react";
+import classes from "./CardCompany.module.css";
+import {load} from "@2gis/mapgl";
+
+
+
+
+const CardCompany = () => {
+    return(
+        <div className={classes.cardComp}>
+            
+        </div>
+    )
+};
+
+export default CardCompany;
