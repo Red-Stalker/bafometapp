618f13e55f1c7c48e9a034bb563fa5fcd017993a
Danirill <dzykin3@gmail.com>
2020-10-24 21:07:09 +0500

    cardd

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -2,7 +2,7 @@ import React from "react"
 import CardCompany from './cardCompany/CardCompany'
 import { InputText } from 'primereact/inputtext';
 import classes from './Sidebar.module.css';
-import Tabs from './Tabs/tabs'
+import Tabs from './Tabs/Tabs'
 
 
 const Sidebar = (props) =>{
--- src/components/Sidebar/cardCompany/CardCompany.js
+++ src/components/Sidebar/cardCompany/CardCompany.js
@@ -1,6 +1,12 @@
 import React, {useContext, useEffect, useState} from "react";
 import classes from "./CardCompany.module.css";
 import {load} from "@2gis/mapgl";
+import {ReactComponent as LogoSave} from "./save.svg";
+import {ReactComponent as LogoShare} from "./share.svg" 
+import {ReactComponent as LogoGo} from "./go.svg";
+import {ReactComponent as LogoStarFull} from "./star1.svg";
+import {ReactComponent as LogoStar} from "./star2.svg";
+
 
 
 
@@ -9,15 +15,18 @@ const CardCompany = () => {
     return(
         <div className={classes.cardComp}>
             <div className={classes.actionCards}>
-                <div className={classes.actionCard}>
-                    
-                </div>  
-                <div className={classes.actionCard}>
-                          
-                </div>
-                <div className={classes.actionCard}>
-                          
-                </div>
+                <span className={classes.actionCard}>
+                    <LogoSave/>
+                    Сохранить
+                </span>
+                <span className={classes.actionCard}>
+                    <LogoShare/>
+                    Отправить
+                </span>
+                <span className={classes.actionCard}>
+                    <LogoGo/>
+                    Проехать
+                </span>
             </div>
             
         </div>
